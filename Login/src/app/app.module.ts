80cdfeef50a4548915706e7fb9b5d90a35c9c6d0
roshanrajvakati <roshanraj.vakati@gmail.com>
2019-08-04 23:26:38 +0530

    first change to repo

---
+++ Login/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
